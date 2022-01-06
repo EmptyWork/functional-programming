def5a644b48a68df17262d2d799823492445f446
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-06 11:05:47 +0900

    Update `StringRepeat.hs` to add repeatStr2

--- haskell/codewars/8kyu/string-repeat/StringRepeat.hs
+++ haskell/codewars/8kyu/string-repeat/StringRepeat.hs
@@ -5,3 +5,14 @@ repeatStr n str = combine (replicate n str)
   where 
     combine [] = []
     combine (x:xs) = combine xs ++ x
+
+repeatStr2 :: Int -> String -> String
+repeatStr2 n str = concat (replicate n str)
+
+{-
+>>> repeatStr2 2 "Hello Guys"
+"Hello GuysHello Guys"
+
+>>> repeatStr2 "Hello Guys"
+"Hello GuysHello Guys"
+-}
