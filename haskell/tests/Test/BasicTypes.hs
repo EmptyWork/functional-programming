2ad41c701032e5c6e501e30c20503c9406d1c726
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-10 21:12:36 +0900

    Update `Test.BasicTypes` - Add more tests

--- haskell/tests/Test/BasicTypes.hs
+++ haskell/tests/Test/BasicTypes.hs
@@ -2,20 +2,23 @@
 
 module Test.BasicTypes where
 
-import Test.Hspec (Spec, describe, it, shouldBe)
+import Test.Hspec (Spec, describe, it, shouldBe, shouldNotBe)
 
 import DataTypes.BasicTypes
-
 
 basicTypes :: Spec
 basicTypes = describe "BasicTypes" $ do
   basicTypes1
 
 basicTypes1 :: Spec
-basicTypes1 = describe "Basic test one: " $ do
+basicTypes1 = describe "BasicTypesNormal" $ do
   it "return True" $ showTrue `shouldBe` True
-  it "return not True so its False" $ not showTrue `shouldBe` False
+  it "return True so its not False" $ showTrue `shouldNotBe` False
+  it "not return True" $ not showTrue `shouldBe` False
+  it "not return True so its False" $ not showTrue `shouldNotBe` True
   it "return 'c' character" $ charC `shouldBe` 'c'
+  it "not return 'j' character" $ charC `shouldNotBe` 'j'
   it "return the number of phi" $ phi `shouldBe` 3.14
   it "return circle area" $ circle 20 `shouldBe` 1256
   it "return circle area" $ circle 2 `shouldBe` 12.56
+  it "return circle area" $ circle 2 `shouldNotBe` 20
