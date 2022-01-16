17c5f70706a57048c08b9133b710df11c946dfd3
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-16 11:32:34 +0900

    Update `datatypes/Main.hs` - include `EmptyWork.hs` and more
    add more print to the terminal which just a showcase of how to
    print more variant things with haskell

--- haskell/basic/Main.hs
+++ haskell/basic/Main.hs
@@ -1,11 +1,20 @@
 module Main (main) where
-import DataTypes.BasicTypes (showTrue, circle, sayHello, charC)
+import DataTypes.BasicTypes (showTrue, circle, sayHello, charC, phi)
+import Data.Typeable (typeOf)
+import EmptyWork (terminalLogo)
 
 main :: IO ()
 main = do
   putStrLn "Copyright (C) 2022  EmptyWork"
   putStrLn ""
+  putStrLn terminalLogo
   sayHello
-  print (circle (2 :: Double))
+  putStr "The area of circle with r = 2 is: "
+  print $ circle (2 :: Double)
+  putStr "The area of circle with r = phi is: "
+  print $ circle (phi :: Double)
+  print [(1 :: Int) .. 10]
+  print [(1 :: Int) .. 200]
   print showTrue
   print charC
+  print $ typeOf charC 
