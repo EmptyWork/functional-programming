9b61ce6baa8faa7038aea60c71ad9df425a0b552
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-16 13:06:58 +0900

    Update `SolveMeFirst.hs` - support args

--- haskell/challenges/solvemefirst/SolveMeFirst.hs
+++ haskell/challenges/solvemefirst/SolveMeFirst.hs
@@ -1,14 +1,27 @@
 module Main where
 import Prelude hiding (sum)
+import System.Environment (getArgs)
 
 solveMeFirst :: Int -> Int -> Int
 solveMeFirst a b = a + b
 
 main :: IO()
 main = do
-    putStrLn "Input the first number: "
-    val1 <- readLn
-    putStrLn "Input the first number: "
-    val2 <- readLn
-    let sum = solveMeFirst val1 val2
-    print sum
+    args <- getArgs
+    case args of
+      [] -> do
+            putStrLn "Input the first number: "
+            val1 <- readLn
+            putStrLn "Input the second number: "
+            val2 <- readLn
+            print $ solveMeFirst val1 val2
+      s : ss -> do
+            let val1 = read s :: Int
+            case ss of 
+              [] -> do
+                    putStrLn "Input the second number: "
+                    val2 <- readLn
+                    print $ solveMeFirst val1 val2
+              str : _ -> do 
+                    let val2 = read str :: Int 
+                    print $ solveMeFirst val1 val2
