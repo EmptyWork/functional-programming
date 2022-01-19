b293e44f449c1373f290ad680705caed3da1d9ef
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-19 20:40:40 +0900

    Update `W3Resource.hs` - Implement `returnMaybe` for `Maybe`

--- haskell/challenges/w3resource/W3Resource.hs
+++ haskell/challenges/w3resource/W3Resource.hs
@@ -1,12 +1,17 @@
 --- https://www.w3resource.com/c-programming-exercises/basic-algo/index.php
 module W3Resource where
 
+import Data.Maybe (fromMaybe)
+
+returnMaybe :: Maybe Int -> Int
+returnMaybe = fromMaybe 0
+
 --- Number one 
 --- Write a Haskell program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.
-sumOfTriple :: Int -> Int -> (Int, Maybe Int)
+sumOfTriple :: Int -> Int -> (Int, Int)
 sumOfTriple numberOne numberTwo
-  | numberOne == numberTwo = (numberOne + numberTwo, Just ((numberOne + numberTwo) * 3))
-  | otherwise = (numberOne + numberTwo, Nothing)
+  | numberOne == numberTwo = (numberOne + numberTwo, returnMaybe $ Just ((numberOne + numberTwo) * 3))
+  | otherwise = (numberOne + numberTwo, returnMaybe Nothing)
 
 --- NumberTwo
 --- Write a Haskell program to get the absolute difference between n and 51. If n is greater than 51 return triple the absolute difference.
