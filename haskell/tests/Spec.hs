1d026311e72990753419142c4a50704cd7040d25
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-25 16:39:24 +0900

    Update `ewhaskell.cabal` and `Spec.hs` - include `ChaW3Re.hs`

--- haskell/tests/Spec.hs
+++ haskell/tests/Spec.hs
@@ -5,9 +5,11 @@ import Test.Hspec (hspec)
 import Test.BasicTypes
 import Test.ObjectTypes
 import Test.Codewars
+import Test.ChaW3Re
 
 main :: IO ()
 main = hspec $ do
   basicTypes
   objectTypes
   codeWars
+  challengesW3Resource 
