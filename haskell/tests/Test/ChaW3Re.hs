e74e2d09ef1617485a8c530ce2e5af69ea51cb1a
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-01-29 12:41:08 +0900

    Update `ChaW3Re.hs` - Add more unit tests

--- haskell/tests/Test/ChaW3Re.hs
+++ haskell/tests/Test/ChaW3Re.hs
@@ -12,20 +12,24 @@ challengesW3Resource = describe "ChallengesW3Resource" $ do
 
 challengesW3Normal :: Spec
 challengesW3Normal = describe "ChallengesW3ResourceNormal" $ do
+
   describe "ChallengesW3NReturnMaybe" $ do
     it "returnMaybe Nothing should be 0" $ returnMaybe Nothing `shouldBe` 0
     it "returnMaybe (Just 89) should be 89" $ returnMaybe (Just 89) `shouldBe` 89
     it "returnMaybe (Just (-8)) should be (-8)" $ returnMaybe (Just (-8)) `shouldBe` (-8)
+
   describe "ChallengesW3NsumOfTriple" $ do
     it "sumOfTriple 20 20 should be (40, 120)" $ sumOfTriple 20 20 `shouldBe` (40, 120)
     it "sumOfTriple 10 11 should be (21, 0)" $ sumOfTriple 10 11 `shouldBe` (21, 0)
     it "sumOfTriple 20 1 should be (21, 0)" $ sumOfTriple 20 1 `shouldBe` (21, 0)
     it "sumOfTriple 1 1 should be (2, 6)" $ sumOfTriple 1 1 `shouldBe` (2, 6)
+
   describe "ChallengesW3NdiffFromFiftyOne" $ do
     it "diffFromFiftyOne 70 should be 57" $ diffFromFiftyOne 70 `shouldBe` 57
     it "diffFromFiftyOne 52 should be 3" $ diffFromFiftyOne 52 `shouldBe` 3
     it "diffFromFiftyOne 30 should be 21" $ diffFromFiftyOne 30 `shouldBe` 21
     it "diffFromFiftyOne 2 should be 49" $ diffFromFiftyOne 2 `shouldBe` 49
+
   describe "ChallengesW3NisThirty" $ do
     it "isThirty 20 20 should be True" $ isThirty 20 20 `shouldBe` False
     it "isThirty 20 20 should not be False" $ isThirty 20 20 `shouldNotBe` True
@@ -33,5 +37,20 @@ challengesW3Normal = describe "ChallengesW3ResourceNormal" $ do
     it "isThirty 20 10 should Not be False" $ isThirty 20 10 `shouldNotBe` False
     it "isThirty 20 30 should Not be False" $ isThirty 20 30 `shouldNotBe` False
     it "isThirty 20 30 should be True" $ isThirty 20 30 `shouldBe` True
-    it "isThirty 30 30 should be False" $ isThirty 20 30 `shouldNotBe` False
-    it "isThirty 30 30 should be True" $ isThirty 20 30 `shouldBe` True
+    it "isThirty 20 30 should be False" $ isThirty 20 30 `shouldNotBe` False
+    it "isThirty 30 20 should be True" $ isThirty 30 20 `shouldBe` True
+    it "isThirty 30 20 should be False" $ isThirty 30 20 `shouldNotBe` False
+    it "isThirty 30 0 should be True" $ isThirty 30 0 `shouldBe` True
+    it "isThirty 30 0 should be False" $ isThirty 30 0 `shouldNotBe` False
+
+  describe "ChallengesW3NisWithinTen" $ do
+    it "isWithinTen of 222" $ isWithinTen 222 `shouldBe` False
+    it "isWithinTen of 222" $ isWithinTen 222 `shouldNotBe` True
+    it "isWithinTen of 110" $ isWithinTen 110 `shouldBe` True
+    it "isWithinTen of 110" $ isWithinTen 110 `shouldNotBe` False
+    it "isWithinTen of 205" $ isWithinTen 205 `shouldNotBe` False
+    it "isWithinTen of 205" $ isWithinTen 205 `shouldBe` True
+  
+  describe "ChallengesW3NisMultipleOfThreeOrSeven" $ do
+    it "isMultipleOfThreeOrSeven" $ isMultipleOfThreeOrSeven 20 `shouldBe` False
+    it "isMultipleOfThreeOrSeven" $ isMultipleOfThreeOrSeven 20 `shouldNotBe` True
