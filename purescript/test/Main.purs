0c41a7839b67fdfe8ea279f8fbaa72258a252698
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-02-06 21:05:11 +0900

    Update `ps:test/Main.purs` converted to `purescript-spec`

--- purescript/test/Main.purs
+++ purescript/test/Main.purs
@@ -3,9 +3,11 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-
+import Effect.Aff (launchAff_)
 import Test.EightKyu (eightKyu)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
-main = do
-  eightKyu
+main = launchAff_ do
+  runSpec [consoleReporter] eightKyu
