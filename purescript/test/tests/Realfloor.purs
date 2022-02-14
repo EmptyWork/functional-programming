14e1fc9197b3623be5d2e17f35dda5100854a4a2
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-02-14 09:03:16 +0900

    Update `ps:RealFloor.purs` more unit tests

--- purescript/test/tests/Realfloor.purs
+++ purescript/test/tests/Realfloor.purs
@@ -8,6 +8,17 @@ import Test.Spec.Assertions (shouldEqual)
 
 testRealFloor ∷ Spec Unit
 testRealFloor = do
-  describe "" do
-    it "" do
+  describe "realFloor: a function that given a floor in the american system returns the floor in the european system" do
+    it "on the zeroth floor" do
       realFloor 0 `shouldEqual` 0
+    it "the floor is lower than 13" do
+      realFloor 3 `shouldEqual` 2
+      realFloor 2 `shouldEqual` 1
+      realFloor 1 `shouldEqual` 0
+      realFloor 12 `shouldEqual` 11
+    it "the floor is higher than 13" do
+      realFloor 14 `shouldEqual` 12
+      realFloor 20 `shouldEqual` 18
+    it "underground floor" do
+      realFloor (-1) `shouldEqual` (-1)
+      realFloor (-12) `shouldEqual` (-12)
