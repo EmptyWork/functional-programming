b013fe9128602eb68eacbb8190044cc056bbaf7b
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-02-14 09:04:11 +0900

    Update `ps:test/EightKyu.purs` include `test/DividedBy.purs`

--- purescript/test/tests/EightKyu.purs
+++ purescript/test/tests/EightKyu.purs
@@ -2,9 +2,11 @@ module Test.EightKyu where
 
 import Prelude
 
+import DividedBy (testDividedBy)
 import Realfloor (testRealFloor)
 import Test.Spec (Spec)
 
 eightKyu :: Spec Unit
 eightKyu = do
   testRealFloor
+  testDividedBy
