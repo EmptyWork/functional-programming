9c3f565c6dc32e4673fbdbd0f24fca860b8e66c2
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-10-18 18:38:25 +0900

    Update `ps:Main` to add `EmptyWork` and `DividedBy`

--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -2,9 +2,13 @@ module Main where
 
 import Prelude
 
+import CodeWars.EightKyu.DividedBy (dividedBy)
 import Effect (Effect)
 import Effect.Console (log)
+import EmptyWork (terminalLogo)
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  log "Copyright (C)  2022 EmptyWork"
+  log $ terminalLogo
+  log $ show (dividedBy 12 2 4)
