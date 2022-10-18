8ddd811bd73057e57c5e2fd5b1f213037fa9f181
emptywork <22065214+EmptyWork@users.noreply.github.com>
2022-10-18 18:40:01 +0900

    Update `hs:DividedBy` to add another way to solve it

--- haskell/codewars/EightKyu/DividedBy/DividedBy.hs
+++ haskell/codewars/EightKyu/DividedBy/DividedBy.hs
@@ -4,3 +4,6 @@ isDividedby :: Integral a => a -> a -> a -> Bool
 isDividedby num a b = isWholeNumber num b && isWholeNumber num a
   where 
     isWholeNumber number divider = number `mod` divider == 0
+
+isDividedby2 :: Int -> Int -> Int -> Bool
+isDividedby2 num a b = (num `mod` a) && (num `mod` b)
